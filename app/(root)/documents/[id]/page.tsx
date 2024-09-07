15a80bc858568241ac1ce46c5a1a4fb2e69f44a4
Rodrigo Pinto <rodrigo.pinto@kyas.com.ar>
2024-09-07 09:01:36 -0300

    Add to active collaborators, actions room, and other features

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,11 +1,31 @@
 
+import CollaborativeRoom from '@/components/CollaborativeRoom'
+import { getDocument } from '@/lib/actions/room.actions';
+import { currentUser } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-const Document = () => {
+const Document = async ({params: {id}}: SearchParamProps) => {
+  const clearkUser = await currentUser();
+  if(!clearkUser) redirect('/sign-in');
+
+  const room = await getDocument({
+    roomId: id,
+    userId: clearkUser.emailAddresses[0].emailAddress
+  });
+  if(!room) redirect('/');
+
+  // TODO: Assess the permissins of the user to access the document.
+  
   return (
-    <div>
-      
-    </div>
+    <main className='flex w-full flex-col items-center'>
+      <CollaborativeRoom 
+        roomId={id}
+        roomMetadata={room.metadata} 
+        users={[]} 
+        currentUserType={'creator'}      
+        />
+    </main>
   )
 }
 
--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -3,22 +3,26 @@ import Header from '@/components/Header'
 import { Editor } from '@/components/editor/Editor'
 import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import { ClientSideSuspense, RoomProvider } from '@liveblocks/react/suspense'
+import ActiveCollaborators from './ActiveCollaborators';
 
-const CollaborativeRoom = () => {
+const CollaborativeRoom = ({roomId, roomMetadata}: CollaborativeRoomProps) => {
     return (
-        <RoomProvider id="my-room">
+        <RoomProvider id={roomId}>
             <ClientSideSuspense fallback={<div>Loading...</div>}>
                 <div className='collaborative-room'>
                     <Header>
                         <div className='flex w-fit items-center justify-center'>
                             <p className='document-title'>Share</p>
                         </div>
-                        <SignedOut>
-                            <SignInButton />
-                        </SignedOut>
-                        <SignedIn>
-                            <UserButton />
-                        </SignedIn>
+                        <div className='flex w-full flex-1 justify-end gap-2 sm:gap-3'>
+                            <ActiveCollaborators />
+                            <SignedOut>
+                                <SignInButton />
+                            </SignedOut>
+                            <SignedIn>
+                                <UserButton />
+                            </SignedIn>
+                        </div>
                     </Header>
                     <Editor />
                 </div>
